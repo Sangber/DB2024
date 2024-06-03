e213c5b2fdc63c5886f8f18522e585672906260d
zuosangber <sangber_mail@mail.ustc.edu.cn>
2024-06-04 00:28:28 +0800

    修改了数据库的创建文件

--- source/SQL/create.sql
+++ source/SQL/create.sql
@@ -1,49 +1,55 @@
-use lab02;
+USE lab02;
 
-create table major (
-	mid char(8),
-    mname varchar(100) check (mname is not null),
-    primary key(mid)
+DROP TABLE IF EXISTS major;
+CREATE TABLE major (
+	mid CHAR(8),
+    mname VARCHAR(100) check (mname is not null),
+    PRIMARY KEY (mid)
 );
 
-create table student (
-	sid char(8),
-    sname varchar(100) check (sname is not null),
-    gender int check (gender = 0 or gender = 1), -- 0表示男，1表示女
-    birth_date date,
-    major_id char(8),
-    foreign key (major_id) references major(mid),
-    primary key (sid)
+DROP TABLE IF EXISTS student;
+CREATE TABLE student (
+	sid CHAR(8),
+    sname VARCHAR(100) check (sname is not null),
+    gender VARCHAR(8) check (gender = '男' or gender = '女'),
+    birth_date DATE,
+    major_id CHAR(8),
+    Foreign Key (major_id) REFERENCES major(mid),
+    PRIMARY KEY (sid)
 );
 
-create table course (
-	cid char(8),
-    cname varchar(100) check (cname is not null),
-    major_id char(8),
-    foreign key (major_id) references major(mid),
-    primary key (cid)
+DROP TABLE IF EXISTS course;
+CREATE TABLE course (
+	cid CHAR(8),
+    cname VARCHAR(100) check (cname is not null),
+    major_id CHAR(8),
+    Foreign Key (major_id) REFERENCES major(mid),
+    PRIMARY KEY (cid)
 );
 
-create table award (
-	aid char(8),
-    aname varchar(100) check (aname is not null),
-    primary key (aid)
+DROP TABLE IF EXISTS award;
+CREATE TABLE award (
+	aid CHAR(8),
+    aname VARCHAR(100) check (aname is not null),
+    PRIMARY KEY (aid)
 );
 
-create table sc (
-	student_id char(8),
-    course_id  char(8),
-    score int check (score >= 0 and score <= 100) default 0,
-    foreign key (student_id) references student(sid),
-    foreign key (course_id)  references course(cid),
-    primary key (student_id, course_id)
+DROP TABLE IF EXISTS sc;
+CREATE TABLE sc (
+	student_id CHAR(8),
+    course_id  CHAR(8),
+    score INT check (score >= 0 and score <= 100) DEFAULT 0,
+    Foreign Key (student_id) REFERENCES student(sid),
+    Foreign Key (course_id)  REFERENCES course(cid),
+    PRIMARY KEY (student_id, course_id)
 );
 
-create table sa (
-	student_id char(8),
-    award_id char(8),
-    award_time date,
-    foreign key (student_id) references student(sid),
-    foreign key (award_id)   references award(aid),
-    primary key (student_id, award_id)
+DROP TABLE IF EXISTS sa;
+CREATE TABLE sa (
+	student_id CHAR(8),
+    award_id CHAR(8),
+    award_time DATE,
+    Foreign Key (student_id) REFERENCES student(sid),
+    Foreign Key (award_id)   REFERENCES award(aid),
+    PRIMARY KEY (student_id, award_id)
 );
