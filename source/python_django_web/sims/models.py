3942ac95ac5e76b1b02a9ff89b1e79eba4fc658c
zuosangber <sangber_mail@mail.ustc.edu.cn>
2024-06-04 14:24:02 +0800

    加入了student

--- source/python_django_web/sims/models.py
+++ source/python_django_web/sims/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class student(models.Model):
+    sid         = models.CharField(max_length = 32, unique = True)
+    sname       = models.CharField(max_length = 32)
+    gender      = models.CharField(max_length = 32)
+    birth_date  = models.CharField(max_length = 32)
+    major_id    = models.CharField(max_length = 32)
