d9d32f9bdbea943a7e4e41de7d5707f89a36837c
zuosangber <sangber_mail@mail.ustc.edu.cn>
2024-06-05 00:06:30 +0800

    新增了专业表

--- source/python_django_web/sims/models.py
+++ source/python_django_web/sims/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class major(models.Model):
+    mid     = models.CharField(max_length = 32, unique = True)
+    mname   = models.CharField(max_length = 32)
+
 class student(models.Model):
     sid         = models.CharField(max_length = 32, unique = True)
     sname       = models.CharField(max_length = 32)
     gender      = models.CharField(max_length = 32)
     birth_date  = models.CharField(max_length = 32)
     major_id    = models.CharField(max_length = 32)
-
-class major(models.Model):
-    mid     = models.CharField(max_length = 32, unique = True)
-    mname   = models.CharField(max_length = 32)
