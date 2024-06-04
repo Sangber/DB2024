95a64c8c7aa7c0d66d47d80d5491cfa4ce9b2359
zuosangber <sangber_mail@mail.ustc.edu.cn>
2024-06-04 14:59:16 +0800

    将url修正为path

--- source/python_django_web/python_django_web/urls.py
+++ source/python_django_web/python_django_web/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.conf.urls import url, include
+from django.conf.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^sims/', include('sims.urls'))
+    path('sims/', include('sims.urls'))
 ]
