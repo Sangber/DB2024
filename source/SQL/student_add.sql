0db045f2bfe1494e291abc6cdd4296bdf26a3fec
zuosangber <sangber_mail@mail.ustc.edu.cn>
2024-06-05 12:43:37 +0800

    新增学生的存储过程

---
+++ source/SQL/student_add.sql
@@ -0,0 +1,15 @@
+DROP PROCEDURE IF EXISTS student_add;
+DELIMITER //
+CREATE PROCEDURE student_add(
+    in sid CHAR(8),
+    in sname VARCHAR(100),
+    in gender VARCHAR(8),
+    in birth_date DATE,
+    in major_id CHAR(8),
+)
+begin
+    INSERT INTO student (sid, sname, gender, birth_date, major_id)
+    VALUES
+    (sid, sname, gender, birth_date, major_id);
+end //
+DELIMITER ;
