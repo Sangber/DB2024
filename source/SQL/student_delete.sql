baa65c95d26362144c5e77250f74268066f15ec2
zuosangber <sangber_mail@mail.ustc.edu.cn>
2024-06-05 17:19:14 +0800

    新增了级联删除

--- source/SQL/student_delete.sql
+++ source/SQL/student_delete.sql
@@ -1,13 +1,17 @@
 -- 删除学生之前，先将其在sc和sa中的记录删除
 -- 使用触发器实现
 
-DROP PROCEDURE IF EXISTS student_delete;
+DROP TRIGGER IF EXISTS cascade_delete;
 DELIMITER //
-CREATE PROCEDURE student_delete(
-    in sid CHAR(8)
-)
-begin
+CREATE TRIGGER cascade_delete BEFORE DELETE ON student FOR EACH ROW
+BEGIN
+    DELETE FROM sc WHERE sc.student_id = OLD.sid;
+    DELETE FROM sa WHERE sa.student_id = OLD.sid;
+END
+
+CREATE PROCEDURE student_delete(in sid CHAR(8))
+BEGIN
     DELETE FROM student
     WHERE s.sid = sid;
-end //
+END //
 DELIMITER ;
