a26bb9949c8dca5f9a35258f8a44d9a65c6452f6
zuosangber <sangber_mail@mail.ustc.edu.cn>
2024-06-05 17:01:48 +0800

    新增了删除学生的存储过程

--- source/SQL/student_delete.sql
+++ source/SQL/student_delete.sql
@@ -1,2 +1,13 @@
 -- 删除学生之前，先将其在sc和sa中的记录删除
 -- 使用触发器实现
+
+DROP PROCEDURE IF EXISTS student_delete;
+DELIMITER //
+CREATE PROCEDURE student_delete(
+    in sid CHAR(8)
+)
+begin
+    DELETE FROM student
+    WHERE s.sid = sid;
+end //
+DELIMITER ;
