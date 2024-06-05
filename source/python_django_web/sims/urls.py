df45db80aff6bd879462667cd9d454a1dd5ca973
zuosangber <sangber_mail@mail.ustc.edu.cn>
2024-06-05 09:38:32 +0800

    新增了课程界面

--- source/python_django_web/sims/urls.py
+++ source/python_django_web/sims/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('major/', views.major_index),
-    path('student/', views.student_index)
+    path('student/', views.student_index),
+    path('course/', views.course_index)
 ]
--- source/python_django_web/sims/views.py
+++ source/python_django_web/sims/views.py
@@ -5,6 +5,23 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'index.html')
 
+def major_index(request):
+    mid         = request.GET.get('mid', '')
+    mname       = request.GET.get('mname', '')
+
+    sql =  "SELECT mid, mname FROM major WHERE 1=1 "
+    if mid.strip() != '':
+        sql = sql + " and mid = '" + mid + "'"
+    if mname.strip() != '':
+        sql = sql + " and mname = '" + mname + "'"
+
+    print(sql)
+    conn = MySQLdb.connect(host="localhost", user="root", passwd="mysql030520", db="lab02", charset='utf8')
+    with conn.cursor(cursorclass = MySQLdb.cursors.DictCursor) as cursor:
+        cursor.execute(sql)
+        majors = cursor.fetchall()
+    return render(request, 'major/index.html', {'majors': majors})
+
 def student_index(request):
     sid         = request.GET.get('sid', '')
     sname       = request.GET.get('sname', '')
@@ -22,19 +39,19 @@ def student_index(request):
         students = cursor.fetchall()
     return render(request, 'student/index.html', {'students': students})
 
-def major_index(request):
-    mid         = request.GET.get('mid', '')
-    mname       = request.GET.get('mname', '')
+def course_index(request):
+    cid         = request.GET.get('cid', '')
+    cname       = request.GET.get('cname', '')
 
     sql =  "SELECT mid, mname FROM major WHERE 1=1 "
-    if mid.strip() != '':
-        sql = sql + " and mid = '" + mid + "'"
-    if mname.strip() != '':
-        sql = sql + " and mname = '" + mname + "'"
+    if cid.strip() != '':
+        sql = sql + " and cid = '" + cid + "'"
+    if cname.strip() != '':
+        sql = sql + " and cname = '" + cname + "'"
 
     print(sql)
     conn = MySQLdb.connect(host="localhost", user="root", passwd="mysql030520", db="lab02", charset='utf8')
     with conn.cursor(cursorclass = MySQLdb.cursors.DictCursor) as cursor:
         cursor.execute(sql)
-        majors = cursor.fetchall()
-    return render(request, 'major/index.html', {'majors': majors})
+        courses = cursor.fetchall()
+    return render(request, 'course/index.html', {'courses': courses})
