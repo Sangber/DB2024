12642d48c513a3b1816636c0361d3046b75ea07a
zuosangber <sangber_mail@mail.ustc.edu.cn>
2024-06-05 18:44:25 +0800

    新增了sa的相关路由

--- source/python_django_web/sims/urls.py
+++ source/python_django_web/sims/urls.py
@@ -5,11 +5,14 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('major/', views.major_index),
-    path('major/edit', views.major_edit),
+    path('major/edit/', views.major_edit),
     path('student/', views.student_index),
-    path('student/add', views.student_add),
-    path('student/edit', views.student_edit),
-    path('student/delete', views.student_delete),
+    path('student/add/', views.student_add),
+    path('student/edit/', views.student_edit),
+    path('student/delete/', views.student_delete),
     path('course/', views.course_index),
-    path('award/', views.award_index)
+    path('award/', views.award_index),
+    path('sa/', views.sa_index),
+    path('sa/add/', views.sa_add),
+    path('sa/delete/', views.sa_delete)
 ]
