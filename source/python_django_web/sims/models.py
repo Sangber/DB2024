30d6b8449b09732eb0e98cbe576cec440a14aead
zuosangber <sangber_mail@mail.ustc.edu.cn>
2024-06-05 09:45:22 +0800

    更新了models

--- source/python_django_web/sims/models.py
+++ source/python_django_web/sims/models.py
@@ -11,3 +11,8 @@ class student(models.Model):
     gender      = models.CharField(max_length = 32)
     birth_date  = models.CharField(max_length = 32)
     major_id    = models.CharField(max_length = 32)
+
+class course(models.Model):
+    cid         = models.CharField(max_length = 32, unique = True)
+    cname       = models.CharField(max_length = 32)
+    major_id    = models.CharField(max_length = 32)
