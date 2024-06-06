2139990bbae1d4e6a5c453fd3408007ba8ddd77d
zuosangber <sangber_mail@mail.ustc.edu.cn>
2024-06-06 17:04:20 +0800

    新增了学生gpa的函数

--- source/SQL/student_gpa.sql
+++ source/SQL/student_gpa.sql
@@ -5,7 +5,7 @@ RETURNS FLOAT
 READS SQL DATA
 BEGIN
     DECLARE course_count INT DEFAULT 0;
-    DECLARE score_total INT DEFAULT 0;
+    DECLARE score_total  INT DEFAULT 0;
     DECLARE gpa FLOAT DEFAULT 0;
     SELECT SUM(score) INTO score_total FROM sc WHERE sc.student_id=sid;
     SELECT COUNT(score) INTO course_count FROM sc WHERE sc.student_id=sid;
