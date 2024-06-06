372dcf7bcda36e35240ddd387e501f93438d1c2d
zuosangber <sangber_mail@mail.ustc.edu.cn>
2024-06-06 17:04:12 +0800

    新增了学生专业gpa的函数

---
+++ source/SQL/student_major_gpa.sql
@@ -0,0 +1,19 @@
+DROP FUNCTION IF EXISTS student_major_gpa;
+DELIMITER //
+CREATE FUNCTION student_major_gpa(sid CHAR(8))
+RETURNS FLOAT
+READS SQL DATA
+BEGIN
+    DECLARE major_course_count INT DEFAULT 0;
+    DECLARE major_score_total  INT DEFAULT 0;
+    DECLARE major_gpa FLOAT DEFAULT 0;
+    SELECT SUM(score) INTO major_score_total
+    FROM sc, course c, student s
+    WHERE sc.student_id=sid and sc.course_id=c.cid and c.major_id=s.major_id and s.sid = sid;
+    SELECT COUNT(score) INTO major_course_count
+    FROM sc, course c, student s
+    WHERE sc.student_id=sid and sc.course_id=c.cid and c.major_id=s.major_id and s.sid = sid;
+    SET major_gpa = major_score_total / major_course_count;
+    RETURN major_gpa;
+END //
+DELIMITER ;
