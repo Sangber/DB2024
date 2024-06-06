57effb9ebcd1103124b61bcdf97bdc8aef030ab1
zuosangber <sangber_mail@mail.ustc.edu.cn>
2024-06-06 21:21:52 +0800

    为各表的主键添加了格式约束

--- source/SQL/create.sql
+++ source/SQL/create.sql
@@ -2,14 +2,14 @@ USE lab02;
 
 DROP TABLE IF EXISTS major;
 CREATE TABLE major (
-	mid CHAR(8),
+	mid CHAR(8) CHECK (REGEXP_LIKE(mid, '^M[0-9]{3}$')),
     mname VARCHAR(100) check (mname is not null),
     PRIMARY KEY (mid)
 );
 
 DROP TABLE IF EXISTS student;
 CREATE TABLE student (
-	sid CHAR(8),
+	sid CHAR(8) CHECK (REGEXP_LIKE(sid, '^S[0-9]{3}$')),
     sname VARCHAR(100) check (sname is not null),
     gender VARCHAR(8) check (gender = '男' or gender = '女'),
     birth_date DATE,
@@ -21,7 +21,7 @@ CREATE TABLE student (
 
 DROP TABLE IF EXISTS course;
 CREATE TABLE course (
-	cid CHAR(8),
+	cid CHAR(8) CHECK (REGEXP_LIKE(cid, '^C[0-9]{3}$')),
     cname VARCHAR(100) check (cname is not null),
     major_id CHAR(8),
     Foreign Key (major_id) REFERENCES major(mid) ON DELETE CASCADE,
@@ -30,7 +30,7 @@ CREATE TABLE course (
 
 DROP TABLE IF EXISTS award;
 CREATE TABLE award (
-	aid CHAR(8),
+	aid CHAR(8) CHECK (REGEXP_LIKE(aid, '^A[0-9]{3}$')),
     aname VARCHAR(100) check (aname is not null),
     PRIMARY KEY (aid)
 );
