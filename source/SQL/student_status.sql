811362bcd6d6d2bf23be0ee59abeb2ad1ce1f74e
zuosangber <sangber_mail@mail.ustc.edu.cn>
2024-06-07 00:07:03 +0800

    补全了触发器的所有情况

--- source/SQL/student_status.sql
+++ source/SQL/student_status.sql
@@ -1,5 +1,6 @@
 DROP TRIGGER IF EXISTS student_status_1;
 DROP TRIGGER IF EXISTS student_status_2;
+DROP TRIGGER IF EXISTS student_status_3;
 DELIMITER //
 CREATE TRIGGER student_status_1
 AFTER INSERT ON sc
@@ -27,6 +28,21 @@ BEGIN
     ELSE
         UPDATE student SET s_status = '合格'
         WHERE sid = NEW.student_id;
+    END IF;
+END //
+CREATE TRIGGER student_status_3
+AFTER DELETE ON sc
+FOR EACH ROW
+BEGIN
+    DECLARE c_count INT DEFAULT 0;
+    SELECT COUNT(*) INTO c_count FROM sc
+    WHERE student_id = OLD.student_id and score < 60;
+    IF c_count >= 3 THEN
+        UPDATE student SET s_status = '不合格'
+        WHERE sid = OLD.student_id;
+    ELSE
+        UPDATE student SET s_status = '合格'
+        WHERE sid = OLD.student_id;
     END IF;
 END //
 DELIMITER ;
