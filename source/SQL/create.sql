d1a7dc03f9f689b3ce57b8977c0ce078ff9f9efe
zuosangber <sangber_mail@mail.ustc.edu.cn>
2024-06-06 16:57:46 +0800

    新增了学生的合格状态

--- source/SQL/create.sql
+++ source/SQL/create.sql
@@ -14,6 +14,7 @@ CREATE TABLE student (
     gender VARCHAR(8) check (gender = '男' or gender = '女'),
     birth_date DATE,
     major_id CHAR(8),
+    s_status VARCHAR(100) check (s_status = '合格' or s_status = '不合格'),
     Foreign Key (major_id) REFERENCES major(mid),
     PRIMARY KEY (sid)
 );
@@ -38,7 +39,7 @@ DROP TABLE IF EXISTS sc;
 CREATE TABLE sc (
 	student_id CHAR(8),
     course_id  CHAR(8),
-    score INT check (score >= 0 and score <= 100) DEFAULT 0,
+    score INT check (score >= 10 and score <= 100) DEFAULT 0,
     Foreign Key (student_id) REFERENCES student(sid),
     Foreign Key (course_id)  REFERENCES course(cid),
     PRIMARY KEY (student_id, course_id)
