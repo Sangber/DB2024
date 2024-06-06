234a01600bc8d1d367a2a7cf9fba095589f2324a
zuosangber <sangber_mail@mail.ustc.edu.cn>
2024-06-06 17:39:03 +0800

    none

--- source/python_django_web/sims/models.py
+++ source/python_django_web/sims/models.py
@@ -1,22 +1,3 @@
 from django.db import models
 
 # Create your models here.
-class major(models.Model):
-    mid     = models.CharField(max_length = 32, unique = True)
-    mname   = models.CharField(max_length = 32)
-
-class student(models.Model):
-    sid         = models.CharField(max_length = 32, unique = True)
-    sname       = models.CharField(max_length = 32)
-    gender      = models.CharField(max_length = 32)
-    birth_date  = models.CharField(max_length = 32)
-    major_id    = models.CharField(max_length = 32)
-
-class course(models.Model):
-    cid         = models.CharField(max_length = 32, unique = True)
-    cname       = models.CharField(max_length = 32)
-    major_id    = models.CharField(max_length = 32)
-
-class award(models.Model):
-    aid     = models.CharField(max_length = 32, unique = True)
-    aname   = models.CharField(max_length = 32)
