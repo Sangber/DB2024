a363d688e8d486527108aa237ea280f6792833a0
zuosangber <sangber_mail@mail.ustc.edu.cn>
2024-06-06 16:57:10 +0800

    新增了求平均成绩的函数

---
+++ source/SQL/student_gpa.sql
@@ -0,0 +1,15 @@
+DROP FUNCTION IF EXISTS student_gpa;
+DELIMITER //
+CREATE FUNCTION student_gpa(sid CHAR(8))
+RETURNS FLOAT
+READS SQL DATA
+BEGIN
+    DECLARE course_count INT DEFAULT 0;
+    DECLARE score_total INT DEFAULT 0;
+    DECLARE gpa FLOAT DEFAULT 0;
+    SELECT SUM(score) INTO score_total FROM sc WHERE sc.student_id=sid;
+    SELECT COUNT(score) INTO course_count FROM sc WHERE sc.student_id=sid;
+    SET gpa = score_total / course_count;
+    RETURN gpa;
+END //
+DELIMITER ;
