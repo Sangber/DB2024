57eac6275b23e4f328cba9803c89c0ce632bc0df
zuosangber <sangber_mail@mail.ustc.edu.cn>
2024-06-07 12:17:25 +0800

    规范了列约束

--- source/SQL/create.sql
+++ source/SQL/create.sql
@@ -3,14 +3,15 @@ USE lab02;
 DROP TABLE IF EXISTS major;
 CREATE TABLE major (
 	mid CHAR(8) CHECK (REGEXP_LIKE(mid, '^M[0-9]{3}$')),
-    mname VARCHAR(100) check (mname is not null),
+    mname VARCHAR(100) not null,
+    logo BLOB,
     PRIMARY KEY (mid)
 );
 
 DROP TABLE IF EXISTS student;
 CREATE TABLE student (
 	sid CHAR(8) CHECK (REGEXP_LIKE(sid, '^S[0-9]{3}$')),
-    sname VARCHAR(100) check (sname is not null),
+    sname VARCHAR(100) not null,
     gender VARCHAR(8) check (gender = '男' or gender = '女'),
     birth_date DATE,
     major_id CHAR(8),
@@ -22,7 +23,7 @@ CREATE TABLE student (
 DROP TABLE IF EXISTS course;
 CREATE TABLE course (
 	cid CHAR(8) CHECK (REGEXP_LIKE(cid, '^C[0-9]{3}$')),
-    cname VARCHAR(100) check (cname is not null),
+    cname VARCHAR(100) not null,
     major_id CHAR(8),
     Foreign Key (major_id) REFERENCES major(mid) ON DELETE CASCADE,
     PRIMARY KEY (cid)
@@ -31,7 +32,7 @@ CREATE TABLE course (
 DROP TABLE IF EXISTS award;
 CREATE TABLE award (
 	aid CHAR(8) CHECK (REGEXP_LIKE(aid, '^A[0-9]{3}$')),
-    aname VARCHAR(100) check (aname is not null),
+    aname VARCHAR(100) ot null,
     PRIMARY KEY (aid)
 );
 
